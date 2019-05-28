e1283dc47311d6916552b56a60d3eed11fa608f7
unknown <samantha.c.egge@gmail.com>
2019-05-28 10:38:15 -0700

    Completed all React interactivity. Installed sass. Need to style all components now.

--- Project-I-Social-Card/social-card/src/components/PostComponents/PostContent.js
+++ Project-I-Social-Card/social-card/src/components/PostComponents/PostContent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Post.scss';
 
 import HeaderContainer from '../HeaderComponents/HeaderContainer'
-import CardContainer from './CardComponents/CardContainer'
+import CardContainer from '../CardComponents/CardContainer'
 import Footer from '../FooterComponents/Footer'
 
 const PostContent = () => {
