63e3a9f124540d79f979da2d6f06477595d0a2c2
unknown <samantha.c.egge@gmail.com>
2019-05-28 09:52:31 -0700

    Began formatting of the Card Container pieces, set up Footer component to be worked on. Set up all import/defaults and base HTML styling. No sass styling of anything is complete. Need to install sass.

--- Project-I-Social-Card/social-card/src/components/PostComponents/PostContent.js
+++ Project-I-Social-Card/social-card/src/components/PostComponents/PostContent.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Post.scss';
 
 import HeaderContainer from '../HeaderComponents/HeaderContainer'
-// import CardContainer from './CardComponents/CardContainer'
+import CardContainer from './CardComponents/CardContainer'
 import Footer from '../FooterComponents/Footer'
 
 const PostContent = () => {
     return(
         <div>
             <HeaderContainer />
-            // <CardContainer />
+            <CardContainer />
             <Footer />
         </div>
     )
